05baad9c39527616e8885816973249b6447666cd
unknown <ibram.medhat2@gmail.com>
2019-10-08 15:26:44 +0200

    Editing problem and adding state abstract class

--- Search Agent/src/Node.java
+++ Search Agent/src/Node.java
@@ -1,11 +1,38 @@
 
 public class Node {
 	
-	//private State currentState;
+	private State currentState;
 	private Node parentNode;
-	private String operator;
+	private char operator;
 	private int depth;
-	private double pathCost;
+	private int pathCost;
+	
+	public Node(State currentState, Node parentNode, char operator, int depth, int pathCost) {
+		super();
+		this.currentState = currentState;
+		this.parentNode = parentNode;
+		this.operator = operator;
+		this.depth = depth;
+		this.pathCost = pathCost;
+	}
+	
+	public State getCurrentState() {
+		return currentState;
+	}
+	public Node getParentNode() {
+		return parentNode;
+	}
+	public char getOperator() {
+		return operator;
+	}
+	public int getDepth() {
+		return depth;
+	}
+	public int getPathCost() {
+		return pathCost;
+	}
+	
+	
 	
 	
 }
--- Search Agent/src/Problem.java
+++ Search Agent/src/Problem.java
@@ -2,14 +2,31 @@ import java.util.ArrayList;
 
 public abstract class Problem {
 	
-	private ArrayList<String> operators;
-	//private State initialState;
+	private String operators;
+	private State initialState;
 	
-	/*public State transitionFunction(State currentState) {
-		
-	}*/
+	public abstract State transitionFunction(State currentState, char operator);
 	
-	//public boolean goalTest(State currentState) {}
+	public abstract boolean goalTest(State currentState);
 	
-	public abstract  double pathCost(String[] operators);
+	public abstract  int pathCost(String operators);
+
+	public String getOperators() {
+		return operators;
+	}
+
+	public void setOperators(String operators) {
+		this.operators = operators;
+	}
+	
+	public State getInitialState() {
+		return initialState;
+	}
+
+	public void setInitialState(State initialState) {
+		this.initialState = initialState;
+	}
+
+	
+	
 }
