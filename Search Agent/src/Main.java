f7e4abb17352c27a71941425dd5cdd88d62b6686
unknown <ibram.medhat2@gmail.com>
2019-11-01 17:00:51 +0200

    Cleaning and adding comments to the main class

--- Search Agent/src/Main.java
+++ Search Agent/src/Main.java
@@ -28,7 +28,6 @@ public class Main {
 		System.out.println("");
 	}
 		
-//		System.out.println(solve(grid7, "BF", false));
 		
 	}
 	
@@ -38,6 +37,7 @@ public class Main {
 		
 		EndGameProblem problem = new EndGameProblem(grid);
 		Node goalNode = null;
+		// here different queueing functions are passed to the general search according to the search strategy
 		switch(strategy) {
 			case "BF":
 				goalNode = generalSearch(problem, QueueingFunction.ENQUEUE_AT_END);break;
@@ -46,8 +46,11 @@ public class Main {
 			
 			case "ID":
 				// Iterative Deepening can not return null node as it is complete
+				// Starting with depth 0
 				int depthLimit = 0;
+				// keep iterating unitl finding a goaltest satisfying node or null node indicating no solution
 				while(goalNode == null || !problem.goalTest(goalNode.getCurrentState())) {
+					// Applying depthLimited search using the general search with - enqueue at front with limit - queueing function
 					goalNode = generalSearch(problem, QueueingFunction.ENQUEUE_AT_FRONT_WITH_LIMIT, depthLimit);
 					depthLimit++;
 				}
@@ -71,42 +74,48 @@ public class Main {
 		
 		System.out.println("Execution Time of "+strategy+": "+((float)(System.currentTimeMillis() - startTime)) / 1000f+" seconds");
 		if(goalNode == null)
+			// If the goal node is null that means that nodes can't be expanded anymore and no goalstate had been reached
 			return "There is no solution";
 		else {
+			// printing the needed operators leading to goal state; the path cost; the number of expanded nodes
 			return ComputeOutput(goalNode);
 		}
 	}
 	public static Node generalSearch(Problem problem, QueueingFunction queueingFunction) {
+		// If the algorithm doesn't include a depth limit it is entered as 0 and not used
 		return generalSearch(problem, queueingFunction, 0);
 	}
 	
 	public static Node generalSearch(Problem problem, QueueingFunction queueingFunction, int depthLimit) {
 		
+		// Initialization of - the expanded nodes and not checked yet - queue
 		Queue<Node> toBeExpandedNodes = null;
 		
 		
-		
+		// If the strategy doesn't include ordered insertion we use normalQueue structure 
 		if(queueingFunction == QueueingFunction.ENQUEUE_AT_FRONT_WITH_LIMIT ||
 				queueingFunction == QueueingFunction.ENQUEUE_AT_END ||
 				queueingFunction == QueueingFunction.ENQUEUE_AT_FRONT) {
 			toBeExpandedNodes = new NormalQueue<Node>();
 		}
+		// If the insertion done in order according to certain criteria priority queue is used
 		else {			
 			toBeExpandedNodes = new PriorityQueueDT<Node>();
 		}
 		
-		//Initialzie VisitedStates
+		//Initialization of VisitedStates
 		problem.getVisitedStates().InitializeList();
 		
 		//enqueue initial state before starting the expanding process
 		Node initialNode = new Node(problem.getInitialState(), null, '\0', 0, 0);
 		
 		Node currentNode = initialNode;
-	
+		
+		// Checking if the node is not goal state node we continue checking and expanding
 		while(currentNode != null && !problem.goalTest(currentNode.getCurrentState())) {
+			// if iterative deepening we check on the depth limit
 			if(currentNode.getDepth() < depthLimit || queueingFunction != QueueingFunction.ENQUEUE_AT_FRONT_WITH_LIMIT) {
 				expand(toBeExpandedNodes, currentNode, problem, queueingFunction);
-//				System.out.println("Expanded!");
 			}
 			if(!toBeExpandedNodes.isEmpty()) {
 				currentNode = (Node) toBeExpandedNodes.removeFirst();
@@ -119,6 +128,7 @@ public class Main {
 		
 	}
 	
+	
 	public static void expand(Queue<Node> toBeExpandedQueue, Node currentNode, Problem problem, QueueingFunction queueingFunction){
 		
 		
@@ -126,27 +136,14 @@ public class Main {
 		ArrayList<Node> childrenNodes = new ArrayList<Node>();
 		for(int i = 0;i < problem.getOperators().length(); i++) {
 			char operator = problem.getOperators().charAt(i); //getting the current expanded operator
+			
+			// Repeated states are handled in the transition function
 			State nextState = problem.transitionFunction(currentNode.getCurrentState(), operator);
 			
 			if(nextState != null)
 			{
-				 //Creating the new node and adding it to the front of the queue
-				// Changing the path cost function to take current node and current operator
-//				System.out.println("Added Child " + i);
-				
-		
-//				System.out.println(nextState);
-				
-				expandedNodes++;
-				//Checking if this state is repeated
-				
-//				if(expandedNodes/ 5000 > lastPrint){
-//					
-//					System.out.println(expandedNodes);
-//					lastPrint++;
-//				}
-				
-				
+				// Adding the expanded nodes according to the transition function and path cost function
+				expandedNodes++;	
 				childrenNodes.add(new Node(nextState, 
 												currentNode, 
 												operator,
@@ -156,6 +153,7 @@ public class Main {
 		
 		}
 		
+		// Deciding on how the expanded nodes enter the queue according to different queueing functions
 		switch (queueingFunction) {
 		case ENQUEUE_AT_END:
 			enqueueAtEnd((NormalQueue<Node>)toBeExpandedQueue, childrenNodes);
@@ -234,60 +232,7 @@ public class Main {
 		}
 	}
 			
-//	public static void orderedInsert(QueueDT<Node> toBeExpandedQueueDT, ArrayList<Node> childrenNodes) {
-//		boolean inserted = false;
-//		for (Node nodeToBeInserted : childrenNodes) {
-//			inserted = false;
-//			int currentCost = nodeToBeInserted.getPathCost();
-//			
-//			int x = currentCost + 1;
-//	        int l = 0, r = toBeExpandedQueueDT.size() - 1;
-//	        int finalM = -1;
-//	        while (l <= r) { 
-//	            int m = l + (r - l) / 2; 
-//	           
-//	            // Check if x is present at mid 
-//	            if (((Node)toBeExpandedQueueDT.getItem(m)).getPathCost() == x) {	            	
-//	            	if(m-1 > 0)
-//	            	{
-//	            		toBeExpandedQueueDT.insertAt(m-1, nodeToBeInserted);
-//	            		inserted = true;
-//	            	}
-//	            	break;
-//	            }
-//	 
-//	            // If x greater, ignore left half 
-//	            if ( ((Node)toBeExpandedQueueDT.getItem(m)).getPathCost()  < x)
-//	                l = m + 1; 
-//	 
-//	            // If x is smaller, ignore right half 
-//	            else
-//	                r = m - 1; 
-//	            
-//	            finalM = m;
-//	        } 
-////			for(int i = 0; i < toBeExpandedQueueDT.size(); i++)
-////			{
-////				Node queueNode = (Node) toBeExpandedQueueDT.getItem(i);
-////				if(queueNode.getPathCost() > currentCost)
-////				{
-////					toBeExpandedQueueDT.insertAt(i, nodeToBeInserted);
-////					inserted = true;
-////					break;
-////				}
-////			}
-//			// if the queue is empty or if nodeToBeInserted Cost is greater than all the queues' nodes
-//			if(!inserted)
-//			{
-//				if(finalM == -1)
-//					toBeExpandedQueueDT.add(nodeToBeInserted);
-//				else
-//					toBeExpandedQueueDT.insertAt(finalM, nodeToBeInserted);
-//			}
-//		}
-//		
-//	}
-	
+	// Computing the cost of goal node and the operators leading to it	
 	public static String ComputeOutput(Node goalNode)
 	{
 		Node currentNode = goalNode;
